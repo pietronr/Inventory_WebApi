005691eeb415ba820b87f6d7c1ecff93d68e4b3c
Pietro Niero Roque <pietro.roque@tgcore.com.br>
2022-04-13 17:36:00 -0300

    ModelBuilder do modelo relacional implementado para as entidades.

--- InventoryManagement_Repository/Sql/InventoryManagementContext.cs
+++ InventoryManagement_Repository/Sql/InventoryManagementContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using InventoryManagement.Entities.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace InventoryManagement.Repository.Sql
 {
@@ -8,15 +9,36 @@ namespace InventoryManagement.Repository.Sql
 
 
         /// <summary>
-        /// Creates a new <see cref="DbContext"/> of <see cref="DomProjectContext"/> with custom settings.
+        /// Creates a new <see cref="DbContext"/> of <see cref="InventoryManagementContext"/> with custom settings.
         /// </summary>
         /// <param name="options"><see cref="DbContext"/> options.</param>
         public InventoryManagementContext(DbContextOptions<InventoryManagementContext> options) : base(options) { }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-           base.OnModelCreating(modelBuilder);
+            modelBuilder
+                .Entity<Product>()
+                .HasMany(x => x.SellingOrders)
+                .WithMany(x => x.SoldProducts)
+                .UsingEntity<ProductSellingOrder>(
+                g => g
+                .HasOne(s => s.SellingOrder)
+                .WithMany(g => g.ProductSellingOrders)
+                .HasForeignKey(s => s.SellingOrderId)
+                .OnDelete(DeleteBehavior.Cascade),
+                g => g
+                .HasOne(s => s.Product)
+                .WithMany(p => p.ProductSellingOrders)
+                .HasForeignKey(s => s.ProductId)
+                .OnDelete(DeleteBehavior.Cascade),
+                g =>
+                {
+                    g.HasKey(s => new { s.SellingOrderId, s.ProductId });
+                });
         }
 
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Seller> Sellers { get; set; }
+        public DbSet<SellingOrder> SellingOrders { get; set; }
     }
 }
