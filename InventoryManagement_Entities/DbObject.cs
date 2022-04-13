0a0e8b3fb5e64e175e900069600ae17b22c8a288
Pietro Niero Roque <pietro.roque@tgcore.com.br>
2022-04-13 10:37:23 -0300

    Criação da solução de entidades, com a classe DbObject. Essa classe define a identificação e rastreabilidade dos objetos no BD.

---
+++ InventoryManagement_Entities/DbObject.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace InventoryManagement_Entities
+{
+    public abstract class DbObject
+    {
+        /// <summary>
+        /// Obtém ou define o ID no banco de dados.
+        /// </summary>
+        public int Id { get; set; }
+    }
+
+    /// <summary>
+    /// Representa uma entidade rastreável do banco de dados.
+    /// </summary>
+    public abstract class Traceable : DbObject
+    {
+        /// <summary>
+        /// Obtém ou define a data de criação da entidade.
+        /// </summary>
+        public DateTime? CreationDate { get; set; }
+
+        /// <summary>
+        /// Obtém ou define a última data de modificação da entidade.
+        /// </summary>
+        public DateTime? LastWriteDate { get; set; }
+    }
+}
