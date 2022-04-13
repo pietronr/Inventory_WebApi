10f0e824b0522c5daa9e1165c9e16439e648b476
Pietro Niero Roque <pietro.roque@tgcore.com.br>
2022-04-13 12:42:03 -0300

    Correção de avisos.

--- InventoryManagement_Repository/Extensions.cs
+++ InventoryManagement_Repository/Extensions.cs
@@ -1,9 +1,5 @@
 ﻿using InventoryManagement_Entities;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace InventoryManagement_Repository
 {
