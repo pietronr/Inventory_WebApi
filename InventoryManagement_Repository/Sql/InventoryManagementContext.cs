fcdcc71d301a0d6db0741ffed38305b19e1f6a8d
Pietro Niero Roque <pietro.roque@tgcore.com.br>
2022-04-13 10:36:26 -0300

    Criação do BDContext.

---
+++ InventoryManagement_Repository/Sql/InventoryManagementContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace InventoryManagement_Repository.Sql
+{
+    public class DomProjectContext : DbContext
+    {
+        public DomProjectContext() { }
+
+
+        /// <summary>
+        /// Creates a new <see cref="DbContext"/> of <see cref="DomProjectContext"/> with custom settings.
+        /// </summary>
+        /// <param name="options"><see cref="DbContext"/> options.</param>
+        public DomProjectContext(DbContextOptions<DomProjectContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+           base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
